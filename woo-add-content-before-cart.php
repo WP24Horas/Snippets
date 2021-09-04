75e29cd184d12668a0f6f876340e5997d269e6f2
Asllan Maciel <contato@asllanmaciel.com.br>
2021-09-04 00:06:32 -0300

    Create woo-add-content-before-cart.php

---
+++ woo-add-content-before-cart.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action('woocommerce_before_cart_contents', 'wp24h_cart_page_custom_text');
+ 
+function wp24h_cart_page_custom_text() {
+ 
+    $message='<div class="cart-custom-message">';
+        $message.='<h2>Delivery of Order</h2>';
+        $message.='<p align="justify">Please note that it may take longer than usual to respond to your concern. Although the option to call us is not available during 9PM to 6AM, we are working hard to help you with your request and would like to thank you for your understanding and patience.</p>';
+    $message.='</div>';
+ 
+    echo $message;
+}
